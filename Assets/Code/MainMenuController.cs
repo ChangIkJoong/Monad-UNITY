551fb12e12fba6c21fe3ef1fbba33fd59242ad15
ik-joong <chang.ikjoong@gmail.com>
2026-02-06 02:24:00 +0100

    add fix

--- Assets/Code/MainMenuController.cs
+++ Assets/Code/MainMenuController.cs
@@ -7,14 +7,12 @@ public class MainMenuController : MonoBehaviour
 
     void Start()
     {
-        // If your gameplay locks the cursor, unlock it in the menu:
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
 
     public void Play()
     {
-        // Safety check: make sure timescale isn't stuck at 0 from pause menus, etc.
         Time.timeScale = 1f;
 
         SceneManager.LoadScene(gameSceneName);
